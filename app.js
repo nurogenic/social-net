2a236cade947685883f805b03912bf3088643ebb
nurogenic <don.desroches@gmail.com>
2014-02-13 09:22:26 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.configure(function(){
+	app.set( 'view engine', 'jade' );
+	app.use( express.static( __dirname + '/public' ) );
+});
+
+app.get('/', function( req, res ){
+	res.render( "index.jade", { layout: false })
+});
+
+app.get('/account/authenticated', function( req, res ) {
+	if( req.session.loggedIn ){
+
+		res.send( 200 );
+
+	} else {
+		res.send( 401 );
+	}
+});
+
+app.listen(8080);
